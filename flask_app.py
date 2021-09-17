ab3ba3bd705610639bad638feea09032f5067597
Dave <DJSaunders1997@gmail.com>
2021-09-17 14:44:29 +0100

    Version 1 of website working

--- flask_app.py
+++ flask_app.py
@@ -114,7 +114,9 @@ def create_pdf(num_pages):
 	# Each page has 2 plots on it
 	# Therefore if 6 people are playing 1 round 3 pages should be printed.
 
-	pp = PdfPages(f'/home/dsaund/blockbusters/blockbuster-{num_pages}-pages.pdf')
+	print('CREATING PDF')
+
+	pp = PdfPages(f'blockbuster-{num_pages}-pages.pdf')
 
 	for i in range(num_pages):
 
@@ -125,29 +127,32 @@ def create_pdf(num_pages):
 
 	pp.close()
 
+	print('PDF CREATED')
 
 app = Flask(__name__)
 
 # How to send files https://pythonprogramming.net/flask-send-file-tutorial/
 # How to read from textbox https://stackoverflow.com/questions/46990497/flask-use-a-button-to-submit-text-in-a-text-box-a-form
 
-@app.route('/', methods=['POST'])
+@app.route('/', methods=['POST', 'GET'])
 def index():
 	#https://flask.palletsprojects.com/en/2.0.x/quickstart/#accessing-request-data
 
 	if request.method == 'POST':
 
-		text = request.form.get('text')
-        	textbox_num = int(text)
-		
+		text = request.form.get('textbox')
+		if text is None or text == '':
+			return('You need to enter a number into the textbox!')
+
+		textbox_num = int(text)
+		num_pages = textbox_num
+		print(num_pages)
+
 		create_pdf(int(num_pages))
-                return send_file(f'blockbuster-{num_pages}-pages.pdf', attachment_filename=f'blockbuster-{num_pages}-pages.pdf')
+		return send_file(f'blockbuster-{num_pages}-pages.pdf', attachment_filename=f'blockbuster-{num_pages}-pages.pdf')
 
 	else:
-
 		return render_template('index.html')
-
-
 
 @app.route('/file-downloads/')
 def file_downloads():
