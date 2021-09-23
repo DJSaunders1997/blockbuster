a367c3c4ac61d4caceba818596b44122927e0826
Dave <DJSaunders1997@gmail.com>
2021-09-23 18:32:22 +0100

    Updating blockbuster function with most uptodate functionality

--- blockbusters.py
+++ blockbusters.py
@@ -45,72 +45,81 @@ coord = [
 
 ]
 
+# Logic functions
+
 # Creates 2 blockbusters plots and saves them to one figure
 def create_2_plots():
-    
-    num_hex = 22
-    num_missing = 3
 
-    randomlist = random.sample(range(1,1+num_hex+num_missing), num_hex)
-    labels = list(map(str, randomlist))
+	num_hex = 22
+	num_missing = 3
 
-    randomlist2 = random.sample(range(1,1+num_hex+num_missing), num_hex)
-    labels2 = list(map(str, randomlist2))
+	randomlist = random.sample(range(1,1+num_hex+num_missing), num_hex)
+	labels = list(map(str, randomlist))
 
-    # Horizontal cartesian coords
-    hcoord = [c[0] for c in coord]
-    # Vertical cartersian coords
-    vcoord = [2. * np.sin(np.radians(60)) * (c[1] - c[2]) /3. for c in coord]
+	randomlist2 = random.sample(range(1,1+num_hex+num_missing), num_hex)
+	labels2 = list(map(str, randomlist2))
 
-    fig, ax = plt.subplots(2,1)
-    ax[0].set_aspect('equal')
-    ax[1].set_aspect('equal')
+	# Horizontal cartesian coords
+	hcoord = [c[0] for c in coord]
+	# Vertical cartersian coords
+	vcoord = [2. * np.sin(np.radians(60)) * (c[1] - c[2]) /3. for c in coord]
 
-    # Add some coloured hexagons
-    for x, y, l in zip(hcoord, vcoord, labels):
-        hex = RegularPolygon((x, y), numVertices=6, radius=2. / 3., 
-                            orientation=np.radians(30), 
-                            facecolor='w', alpha=0.2, edgecolor='k')
-        ax[0].add_patch(hex)
+	fig, ax = plt.subplots(2,1)
+	ax[0].set_aspect('equal')
+	ax[1].set_aspect('equal')
 
-        # Also add a text label
-        ax[0].text(x, y+0.2, l, ha='center', va='center', size=8)
+	# Add some coloured hexagons
+	for x, y, l in zip(hcoord, vcoord, labels):
+		hex = RegularPolygon((x, y), numVertices=6, radius=2. / 3.,
+							orientation=np.radians(30),
+							facecolor='w', alpha=0.2, edgecolor='k')
+		ax[0].add_patch(hex)
 
-    # Also add scatter points in hexagon centres
-    ax[0].scatter(hcoord, vcoord, alpha=0) # Adds invisible centroids, not sure why its needed but it is
+		# Also add a text label
+		ax[0].text(x, y+0.2, l, ha='center', va='center', size=8)
 
-    # Add some coloured hexagons
-    for x, y, l in zip(hcoord, vcoord, labels2):
-        hex = RegularPolygon((x, y), numVertices=6, radius=2. / 3., 
-                            orientation=np.radians(30), 
-                            facecolor='w', alpha=0.2, edgecolor='k')
-        ax[1].add_patch(hex)
+	# Also add scatter points in hexagon centres
+	ax[0].scatter(hcoord, vcoord, alpha=0) # Adds invisible centroids, not sure why its needed but it is
 
-        # Also add a text label
-        ax[1].text(x, y+0.2, l, ha='center', va='center', size=8)
+	# Add some coloured hexagons
+	for x, y, l in zip(hcoord, vcoord, labels2):
+		hex = RegularPolygon((x, y), numVertices=6, radius=2. / 3.,
+							orientation=np.radians(30),
+							facecolor='w', alpha=0.2, edgecolor='k')
+		ax[1].add_patch(hex)
 
-    # Also add scatter points in hexagon centres
-    ax[1].scatter(hcoord, vcoord, alpha=0) # Adds invisible centroids, not sure why its needed but it is
+		# Also add a text label
+		ax[1].text(x, y+0.2, l, ha='center', va='center', size=8)
 
-    ax[0].axis('off') # Remove unnecessary axis
-    ax[1].axis('off') # Remove unnecessary axis
+	# Also add scatter points in hexagon centres
+	ax[1].scatter(hcoord, vcoord, alpha=0) # Adds invisible centroids, not sure why its needed but it is
 
-    return fig
+	ax[0].axis('off') # Remove unnecessary axis
+	ax[1].axis('off') # Remove unnecessary axis
+
+	plt.close(fig)	# Close fig to produce more than 20 and save memory
+
+	return fig
 
 def create_pdf(num_pages):
-    # Each page has 2 plots on it
-    # Therefore if 6 people are playing 1 round 3 pages should be printed.
+	# Each page has 2 plots on it
+	# Therefore if 6 people are playing 1 round 3 pages should be printed.
 
-    pp = PdfPages(f'blockbuster-{num_pages}-pages.pdf')
+	print('CREATING PDF')
 
-    for i in range(num_pages):
+	pp = PdfPages(f'blockbuster-{num_pages}-pages.pdf')
 
-        plot = create_2_plots()
-        plot.set_size_inches([7,10])
+	for i in range(num_pages):
 
-        pp.savefig(plot)
+		plot = create_2_plots()
+		plot.set_size_inches([7,10])
 
-    pp.close()
+		pp.savefig(plot)
 
+	pp.close()
 
-create_pdf(4)
+	print('PDF CREATED')
+
+# Test functions
+# create_pdf(4)
+
